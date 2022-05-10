e500ea1bd96853bcd6c1f587aa345f654e843c4b
Arthur Rocha <91231500+ar7hurr0och4@users.noreply.github.com>
2022-05-09 22:12:05 -0300

    tela login

--- materialize/js/login.js
+++ materialize/js/login.js
@@ -38,22 +38,3 @@ function checkInputs() {
 
 
 }
-
-function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small')
-
-    small.innerText = message
-
-    formControl.className = 'form-control error'
-}
-
-function setSuccessFor(input) {
-    const formControl = input.parentElement;
-
-    formControl.className = 'form-control success'
-}
-
-function isEmail(email) {
-    return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
-}
