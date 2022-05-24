abd41a972e4bd5c9d6b4318ce78ffa8a5abc1a6c
Mateus Melo <mateuscmelo@live.com>
2022-05-23 22:36:21 -0300

    Fixed body format for login.

--- src/back-end/AdoptApi/Requests/UserLoginRequest.cs
+++ src/back-end/AdoptApi/Requests/UserLoginRequest.cs
@@ -2,6 +2,7 @@ using AdoptApi.Requests.Dtos;
 
 namespace AdoptApi.Requests;
 
-public class UserLoginRequest: BaseUserRequestDto
+public class UserLoginRequest
 {
+    public BaseUserRequestDto User { get; set; }
 }
