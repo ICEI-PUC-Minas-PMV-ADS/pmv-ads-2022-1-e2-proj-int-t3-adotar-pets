bccf1e9a9fda3e1ea17c6c290bc8663cad3fea70
Pedro Thiago <pthyago212@gmail.com>
2022-05-29 22:47:52 -0300

    WIP - Ediçao de perfil + cadastro pet

--- materialize/js/cadastropet.js
+++ materialize/js/cadastropet.js
@@ -1,17 +1,103 @@
-var file = document.getElementById("image-input");
-var uploaded_image = "";
+import { setFieldError, Validate } from '../../assets/js/forms/validation.js';
 
-function inserirImagem() {
+
+
+
+
+const registerForm = document.querySelector('[data-register]');
+
+var file = document.getElementById("image-input");
+var imgPet = "";
+var pet;
+
+// Funcionar os selects //
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems,);
+});
+
+init();
+  
+function init(){
+
+  pet = {
+    Nome: null,
+    Idade: null,
+    Tipo: null,
+    Genero: null,
+    Porte: null,
+    Condicao: null,
+    Descricao: null,
+  };
+
+}
+
+// var pet = {
+//   Nome: document.getElementById("nome-pet"),
+//   Idade: document.getElementById("idade-pet"),
+//   Tipo: document.getElementById("tipo-pet"),
+//   Genero: document.getElementById("genero-pet"),
+//   Porte: document.getElementById("porte-pet"),
+//   Condicao: document.getElementById("condicoes-pet"),
+//   Descricao: document.getElementById("descricao-pet"),
+// }
+
+registerForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  var inputNome = document.getElementById("nome-pet");
+  var inputIdade = document.getElementById("idade-pet");
+  var inputTipo = document.getElementById("tipo-pet");
+  var inputGenero = document.getElementById("genero-pet");
+  var inputPorte = document.getElementById("porte-pet");
+  var inputCondicoes = document.getElementById("condicoes-pet");
+  var inputDescricao = document.getElementById("descricao-pet");
+
+  var erros = false;
+
+  let inputs = registerForm.querySelectorAll('input[name], select[name], textarea[name]');
+
+  inputs.forEach((input)=>{
+    try {
+      let validatedInputNome = new Validate(input).required();  
+    } catch (e) {
+      erros = true;
+    }
+  });
+
+  
+  if (!erros){
+    alert("chegou aqui")
+
+    var body = {
+      user: {
+          name: document.getElementById("nome-pet"),
+          email: document.getElementById("idade-pet"),
+          birthDate: document.getElementById("genero-pet"),
+          phone: document.getElementById("porte-pet"),
+          condicoes: document.getElementById("condicoes-pet"),
+          descricao: document.getElementById("descricao-pet"),
+          imagem1: document.getElementById("image-input1"),
+          imagem2: document.getElementById("image-input2"),
+          imagem3: document.getElementById("image-input3"),
+      },    
+     }
+  };
+
+});
+
+
+function inserirImagem(){      
   file.click();
 
   file.addEventListener("change", function () {
     const reader = new FileReader();
 
     reader.addEventListener("load", () => {
-      uploaded_image = reader.result;
-      document.getElementById(
-        "output_image"
-      ).style.backgroundImage = `url(${uploaded_image})`;
+      this.imgPet = reader.result;
+      document.getElementById("output_image").style.backgroundImage = `url(${imgPet})`;
+      pet.imagem1 = imgPet;
     });
 
     reader.readAsDataURL(this.files[0]);
--- materialize/js/perfil-adotante.js
+++ materialize/js/perfil-adotante.js
@@ -1,3 +1,5 @@
+import { setFieldError, Validate } from '../../assets/js/forms/validation.js';
+import {api} from '../api/client.js';
 
 var file = document.getElementById("image-input");
 var imgUsuario ="";
@@ -9,9 +11,6 @@ function inserirImagem(){
     file.click();
 
     file.addEventListener("change", function(){
-
-    
-
 
         const reader = new FileReader();
 
@@ -26,47 +25,47 @@ function inserirImagem(){
     });
 }
 
-function carregarPerfilAdotante(){
+// function carregarPerfilAdotante(){
 
 
-  var usuarioResponse = {
-    "id": 10,
-    "name": "Testevaldo",
-    "email": "testevaldo@teste.com",
-    "birthDate": "01-01-2000",
-    "document": {
-        "type": 0,
-        "number": "31765554667"
-    },
-    "phone": "33912345678",
-    "address": {
-        "city": "Contagem",
-        "name": "Rua VP-2",
-        "zipCode": "32050080"
-    }
-    }
+//   var usuarioResponse = {
+//     "id": 10,
+//     "name": "Testevaldo",
+//     "email": "testevaldo@teste.com",
+//     "birthDate": "01-01-2000",
+//     "document": {
+//         "type": 0,
+//         "number": "31765554667"
+//     },
+//     "phone": "33912345678",
+//     "address": {
+//         "city": "Contagem",
+//         "name": "Rua VP-2",
+//         "zipCode": "32050080"
+//     }
+//     }
 
-    this.usuario = usuarioResponse;
+//     this.usuario = usuarioResponse;
 
 
-    var inputNome = document.getElementById("nome");
-    inputNome.value = usuarioResponse.name;
+//     var inputNome = document.getElementById("nome");
+//     inputNome.value = usuarioResponse.name;
 
-    var inputDocument = document.getElementById("document");
-    inputDocument.value = usuarioResponse.document.number;
+//     var inputDocument = document.getElementById("document");
+//     inputDocument.value = usuarioResponse.document.number;
     
-    var inputBirthDate = document.getElementById("birth-date");
-    inputBirthDate.value = usuarioResponse.birthDate;
+//     var inputBirthDate = document.getElementById("birth-date");
+//     inputBirthDate.value = usuarioResponse.birthDate;
 
-    var inputEmail = document.getElementById("email");
-    inputEmail.value = usuarioResponse.email;
+//     var inputEmail = document.getElementById("email");
+//     inputEmail.value = usuarioResponse.email;
 
-    var inputPhone = document.getElementById("phone");
-    inputPhone.value = usuarioResponse.phone;
+//     var inputPhone = document.getElementById("phone");
+//     inputPhone.value = usuarioResponse.phone;
 
-    var inputCep = document.getElementById("cep");
-    inputCep.value = usuarioResponse.address.zipCode;
-}
+//     var inputCep = document.getElementById("cep");
+//     inputCep.value = usuarioResponse.address.zipCode;
+// }
 
 
 
@@ -75,55 +74,72 @@ buttonSalvarPerfil.addEventListener('click', async event => {
     
     event.preventDefault();
 
-    var inputNome = document.getElementById("nome");
-    var inputEmail = document.getElementById("email");
-    var erros = 0;
+    // var inputNome = document.getElementById("nome");
+    // var inputEmail = document.getElementById("email");
+    // var erros = false;
+    let inputs = registerForm.querySelectorAll('input[name]');
 
-    if (!validationEmpty(inputEmail)){
-        erros++
-    }
-    if (!validationEmpty(inputNome)){
-        erros++
-    }
-    
-    if (erros === 0){
+    inputs.forEach((input)=>{
+        try {
+          let validatedInputNome = new Validate(input).required(); 
+          let validatedInputEmail = new Validate(input).required().email(); 
+        } catch (e) {
+          erros = true;
+        }
+      });
 
+      if (!erros){
         var body = {
-            user: {
-                name: document.getElementById("nome"),
-                email: document.getElementById("email"),
-                birthDate: this.usuario.birthDate,
-                phone: this.usuario.phone,
-                imagem: this.imgUsuario === "" ? this.usuario.imagem : this.imgUsuario 
-            },
-            document: {
-                type: this.usuario.document.type,
-                number: this.usuario.document.number,
-            },
-            address: {
-                zipCode: this.usuario.zipCode,
-            },
+            User: {
+                Name: inputNome,
+                Email: inputEmail      
+            }
+         };
+
+         salvar(body);
+
+      };
+    
+
+});
+//     if (!validationEmpty(inputEmail)){
+
+//     }
+//     if (!validationEmpty(inputNome)){
+//         erros++
+//     }
+    
+//     if (erros === 0){
+
+//         var body = {
+//             user: {
+//                 name: document.getElementById("nome"),
+//                 email: document.getElementById("email"),
+//                 // birthDate: this.usuario.birthDate,
+//                 // phone: this.usuario.phone,
+//                 // imagem: this.imgUsuario === "" ? this.usuario.imagem : this.imgUsuario 
+//             },
+//             // document: {
+//             //     type: this.usuario.document.type,
+//             //     number: this.usuario.document.number,
+//             // },
+//             // address: {
+//             //     zipCode: this.usuario.zipCode,
+//             // },
 
             
-        }
-    }   
-});
+//         }
+//     }   
+// });
 
 
 async function salvar(body){
 
-    const apiUrl = 'https://adoptapi.azurewebsites.net/api/user';
-    const settings = {
-        method: 'PUT',
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        },
-        body: JSON.stringify(body),
-    };
-    try {
-        //tente isso
+    const apiUrl = 'user/profile';
 
-        const response = await fetch(apiUrl, settings);
+    try {
+
+        const response = api.post('user/profile', body, true);
         
         if (!response.ok) {
             throw response;
@@ -137,9 +153,4 @@ async function salvar(body){
         alert("Ocorreu um erro ao salvar.");
     }
 
-}
-
-
-
-
-   
+};
