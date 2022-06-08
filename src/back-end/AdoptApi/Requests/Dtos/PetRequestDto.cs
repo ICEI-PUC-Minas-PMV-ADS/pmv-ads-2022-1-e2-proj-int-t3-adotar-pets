fbb0d2f0a77662e8af7dd620999c17b6c2828a9c
Brunnalage <brunnalage@gmail.com>
2022-06-08 00:38:29 -0300

    ValidateNeed

--- src/back-end/AdoptApi/Requests/Dtos/PetRequestDto.cs
+++ src/back-end/AdoptApi/Requests/Dtos/PetRequestDto.cs
@@ -6,21 +6,33 @@ namespace AdoptApi.Requests.Dtos;
 
 public class PetRequestDto : IValidatableObject
 {
-    [Required(ErrorMessage ="O nome do animal é obrigatório.")]
+    [Required(ErrorMessage = "O nome do animal é obrigatório.")]
     public string Name { get; set; }
-    [Required(ErrorMessage ="O tipo do animal é obrigatório.")]
+    [Required(ErrorMessage = "O tipo do animal é obrigatório.")]
     public PetType Type { get; set; }
-    [Required(ErrorMessage ="O gênero do animal é obrigatório")]
+    [Required(ErrorMessage = "O gênero do animal é obrigatório")]
     public PetGender Gender { get; set; }
-    [Required(ErrorMessage ="A idade do animal é obrigatória, ainda que aproximada")]
+    [Required(ErrorMessage = "A idade do animal é obrigatória, ainda que aproximada")]
     public string BirthDate { get; set; }
-    [Required(ErrorMessage ="O porte do animal é obrigatório")]
+    [Required(ErrorMessage = "O porte do animal é obrigatório")]
     public PetSize Size { get; set; }
-    [Required(ErrorMessage ="A pontuação para adoção do animal é obrigatório.(O MinScore é a pontuação mínima de requisitos que o candidato adotante deve ter para adotar este animal")]
+    [Required(ErrorMessage = "A pontuação para adoção do animal é obrigatório.(O MinScore é a pontuação mínima de requisitos que o candidato adotante deve ter para adotar este animal")]
     public int MinScore { get; set; }
-    [Required(ErrorMessage ="A descrição do animal é obrigatória. Conte um pouco sobre o pet para que o adotante saiba um pouco mais sobre ele.")]
+    [Required(ErrorMessage = "A descrição do animal é obrigatória. Conte um pouco sobre o pet para que o adotante saiba um pouco mais sobre ele.")]
     public string Description { get; set; }
-    public ICollection<Need> Needs { get; set; }      
+    public ICollection<Need> Needs { get; set; }
+
+    public static bool ValidateNeed(int[] Needs)
+    {
+        foreach (int need in Needs)
+        {
+            if (!Needs.Contains(need))
+            {                
+               return false;
+            }
+        }
+        return true;
+    }
 
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
@@ -37,13 +49,13 @@ public class PetRequestDto : IValidatableObject
         if (birthDate == null)
         {
             yield return new ValidationResult("O pet deve ter uma idade válida.",
-                new List<string> {nameof(BirthDate)});
+                new List<string> { nameof(BirthDate) });
         }
-        
+
         if (DateOnly.FromDateTime(DateTime.Now).CompareTo(birthDate) < 0)
         {
             yield return new ValidationResult("O pet deve ter mais de 0 anos.",
-                new List<string> {nameof(BirthDate)});
+                new List<string> { nameof(BirthDate) });
         }
     }
 }
