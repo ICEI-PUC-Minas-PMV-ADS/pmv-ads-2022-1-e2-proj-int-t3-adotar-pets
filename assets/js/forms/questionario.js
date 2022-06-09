2e70e161ba3c4bd951b8059db09bc8af7e66184f
PedJump <pedro.santos.1363251@sga.pucminas.br>
2022-06-09 02:24:15 -0300

    Wip correções

---
+++ assets/js/forms/questionario.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var cars = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(cars, {
+    dist: 0,
+    duration: 100,
+    indicators: true
+  })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems,);
+});
+
+
+
+var instance = M.Carousel.getInstance(elem);
+
+
+
+instance.next('btn');
+instance.next(1); // Mover as próximas n vezes.
+
+instance.prev();
+instance.prev(3);
+
+
+
+var instance = M.Carousel.init({
+  fullWidth: true,
+  indicators: true
+});
